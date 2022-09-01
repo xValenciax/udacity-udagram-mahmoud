39d73e7f053c5ede9cac762690af814cab3ad507
Selim <mamo31st@gmail.com>
2022-09-01 12:27:17 +0200

    test commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export POSTGRES_PASSWORD=151101
+export POSTGRES_HOST=127.0.0.1
+export POSTGRES_DB=udagram
+export AWS_BUCKET=arn:aws:s3:::my-9418-3495-4835-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export JWT_SECRET=elmoreGumball
+export URL=http://localhost
